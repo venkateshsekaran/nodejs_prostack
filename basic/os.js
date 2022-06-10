248e62c227adb7f4f62e998ca5874a288d1e0877
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-06-10 16:13:33 +0530

    add

--- basic/os.js
+++
@@ -1,7 +0,0 @@
-const os = require("os");
-console.log(os.homedir());
-console.log(os.freemem());
-console.log(os.platform());
-console.log(os.userInfo());
-console.log(os.type());
-console.log(os.uptime());
